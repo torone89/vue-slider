fc334f134d82edb45ad2b37b32f52de36d0133ae
torone89 <torone89>
2022-07-12 15:06:42 +0200

    Bottoni Prima e Dopo OK

--- js/script.js
+++ js/script.js
@@ -20,25 +20,25 @@ Vue.config.devtools = true
 // 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente (questo richiederà qualcosa che non abbiamo visto)
 // 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce (questo richiederà degli eventi che non abbiamo visto)
 
-const slider = new Vue ({
+const slider = new Vue({
     el: '#root',
     data: {
         currentIndex: 0,
-        
-        immagini: [ {
+
+        immagini: [{
             url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
             title: 'Svezia',
             description:
                 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
         },
-    
+
         {
             url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
             title: 'Perù',
             description:
                 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
         },
-    
+
         {
             url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
             title: 'Chile',
@@ -57,9 +57,32 @@ const slider = new Vue ({
             description:
                 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
         },]
-       
+
     },
-    methods: {}
+    methods: {
+        goNext() {
+
+            // RIPARTO DA 0 quando arrivo in fondo alle immagini
+
+            if (this.currentIndex == this.immagini.length - 1) {
+
+                this.currentIndex = 0;
+            } else {
+                //     // incremento activindex 
+                this.currentIndex++;
+            }
+        },
+        goBack() {
+
+            if (this.currentIndex == 0) {
+                this.currentIndex = this.immagini.length - 1
+            } else {
+                this.currentIndex--;
+            }
+        },
+
+
+    }
 })
 
 
