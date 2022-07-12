5702fd0de57367122c840b8eefd3bb7ba8a18a32
torone89 <torone89>
2022-07-12 16:46:16 +0200

    Bonus 2: Ok

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ Vue.config.devtools = true
 // Al click sulle freccette l'immagine principale deve cambiare (e la thumbnail corrispondente deve avere la classe active con conseguente effetto visivo)
 // Implementare il ciclo infinito: se sono sulla prima immagine e clicco prev, devo ricominciare dall'ultima. Se sono sull'ultima e clicco next, devo ripartire dalla prima.
 // Bonus:
+
 // 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
 // 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente (questo richiederà qualcosa che non abbiamo visto)
 // 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce (questo richiederà degli eventi che non abbiamo visto)
@@ -24,7 +25,7 @@ const slider = new Vue({
     el: '#root',
     data: {
         currentIndex: 0,
-
+        time: 0,
         immagini: [{
             url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
             title: 'Svezia',
@@ -71,12 +72,18 @@ const slider = new Vue({
             }
         },
         goBack() {
-            if (this.currentIndex == 0) {
+            this.currentIndex--;
+            if (this.currentIndex < 0)
                 this.currentIndex = this.immagini.length - 1
-            } else {
-                this.currentIndex--;
-            }
         },
+        // SLIDER AUTO TIME
+        autoplay: function () {
+            this.time = setInterval(this.goNext, 3000)
+        },
+    },
+    // mounted() : viene eseguito prima di creare il componente.
+    mounted() {
+        this.autoplay()
     }
 })
 
